74d9e773fb0c2a5c342365507d231092cea47476
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2022-06-22 11:39:08 +0200

    the three search methods were created at usercontroller

--- Manager.API/Controllers/UserController.cs
+++ Manager.API/Controllers/UserController.cs
@@ -109,5 +109,137 @@ namespace Manager.API.Controllers
                 return StatusCode(500, Responses.ApplicationErrorMessage());
             }
         }
+
+        [HttpPut]
+        [Route("/api/v1/users/update-user")]
+        public async Task<IActionResult> UpdateUser([FromBody] UpdateUserViewModel userViewModel)
+        {
+            try
+            {
+                var userDTO = _mapper.Map<UserDTO>(userViewModel);
+
+                var userUpdated = await _userService.UpdateUserAsync(userDTO);
+
+                return Ok(new ResultViewModel
+                {
+                    Message = "Usuário atualizado com sucesso!",
+                    Sucess = true,
+                    Data = userUpdated
+                });
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(Responses.DomainErrorMessage(ex.Message, ex.Errors));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("/api/v1/users/get-by-email")]
+        public async Task<IActionResult> GetByEmail([FromQuery] string email)
+        {
+            try
+            {
+                var user = await _userService.GetByEmail(email);
+
+                if (user == null)
+                {
+                    return Ok(new ResultViewModel
+                    {
+                        Message = "Nenhum usuário foi encontrado com o email informado.",
+                        Sucess = true,
+                        Data = user
+                    });
+                }
+
+                return Ok(new ResultViewModel
+                {
+                    Message = "Usuário encontrado com sucesso!",
+                    Sucess = true,
+                    Data = user
+                });
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(Responses.DomainErrorMessage(ex.Message, ex.Errors));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, Responses.ApplicationErrorMessage());
+            }
+        }
+
+        [HttpGet]
+        [Route("/api/v1/users/search-by-email")]
+        public async Task<IActionResult> SearchByEmail([FromQuery] string email)
+        {
+            try
+            {
+                var allUsers = await _userService.SearchByEmail(email);
+
+                if (allUsers.Count == 0)
+                {
+                    return Ok(new ResultViewModel
+                    {
+                        Message = "Nenhum usuário foi encontrado com o email informado.",
+                        Sucess = true,
+                        Data = null
+                    });
+                }
+
+                return Ok(new ResultViewModel
+                {
+                    Message = "Usuário encontrado com sucesso!",
+                    Sucess = true,
+                    Data = allUsers
+                });
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest(Responses.DomainErrorMessage(ex.Message, ex.Errors));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, Responses.ApplicationErrorMessage());
+            }
+        }
+
+        [HttpGet]
+        [Route("/api/v1/users/search-by-name")]
+        public async Task<IActionResult> SearchByName([FromQuery] string name)
+        {
+            try
+            {
+                var allUsers = await _userService.SearchByName(name);
+
+                if (allUsers.Count == 0)
+                {
+                    return Ok(new ResultViewModel
+                    {
+                        Message = "Nenhum usuário foi encontrado com o nome informado",
+                        Sucess = true,
+                        Data = null
+                    });
+                }
+
+                return Ok(new ResultViewModel
+                {
+                    Message = "Usuário encontrado com sucesso!",
+                    Sucess = true,
+                    Data = allUsers
+                });
+            }
+            catch (DomainException ex)
+            {
+                return Ok(Responses.DomainErrorMessage(ex.Message, ex.Errors));
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, Responses.ApplicationErrorMessage());
+            }
+        }
     }
 }

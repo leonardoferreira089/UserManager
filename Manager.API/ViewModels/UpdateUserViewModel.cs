b6ff76f824863226e341cf4efd15fd732892e50f
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2022-06-22 11:33:24 +0200

    UpdateUser was created at UserController

---
+++ Manager.API/ViewModels/UpdateUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Manager.API.ViewModels
+{
+    public class UpdateUserViewModel
+    {
+    }
+}
